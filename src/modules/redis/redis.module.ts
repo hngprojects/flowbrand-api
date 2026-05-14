75b5daccd60a77db38ab946104c9e3d32c0143f8
elijah arhinful <elijaharhinful8@gmail.com>
2026-05-14 20:59:16 +0000

    chore(auth-guard): rename jwt-guard to auth guard in app module

--- src/modules/redis/redis.module.ts
+++ src/modules/redis/redis.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { RedisController } from './redis.controller';
 import { RedisService } from './redis.service';
 
 @Module({
   providers: [RedisService],
-  controllers: [RedisController],
 })
 export class RedisModule {}
