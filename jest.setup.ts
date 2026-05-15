d6724f37579c856f556a4231c5a49ce52e8bdd77
Nockk <ob.ogheneochuko@gmail.com>
2026-05-15 23:16:48 +0100

    chore: get ci to pick jest

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,28 +1,23 @@
-NODE_ENV=development
-PORT=3000
-
-# PostgreSQL
-DATABASE_HOST=localhost
-DATABASE_PORT=5432
-DATABASE_USER=postgres
-DATABASE_PASSWORD=postgres
-DATABASE_NAME=nestjs_starter
-DATABASE_SYNC=false
-DATABASE_LOGGING=false
-DATABASE_SSL=false
-
-# JWT (must be at least 32 characters each)
-JWT_ACCESS_SECRET=change-me-to-a-long-random-string-min-32-chars
-JWT_ACCESS_EXPIRES_IN=15m
-JWT_REFRESH_SECRET=change-me-too-to-a-different-long-random-string
-JWT_REFRESH_EXPIRES_IN=7d
-
-# Misc
-CORS_ORIGIN=*
-SWAGGER_ENABLED=true
-
-# Redis
-REDIS_HOST=localhost
-REDIS_PORT=6379
-REDIS_PASSWORD=
-REDIS_USERNAME=
+// jest.setup.ts
+process.env.NODE_ENV = 'test';
+process.env.PORT = '3000';
+process.env.DATABASE_HOST = 'localhost';
+process.env.DATABASE_PORT = '5432';
+process.env.DATABASE_USER = 'postgres';
+process.env.DATABASE_PASSWORD = 'postgres';
+process.env.DATABASE_NAME = 'nestjs_starter';
+process.env.DATABASE_SYNC = 'false';
+process.env.DATABASE_LOGGING = 'false';
+process.env.DATABASE_SSL = 'false';
+process.env.JWT_ACCESS_SECRET =
+  'change-me-to-a-long-random-string-min-32-chars';
+process.env.JWT_ACCESS_EXPIRES_IN = '15m';
+process.env.JWT_REFRESH_SECRET =
+  'change-me-too-to-a-different-long-random-string';
+process.env.JWT_REFRESH_EXPIRES_IN = '7d';
+process.env.CORS_ORIGIN = '*';
+process.env.SWAGGER_ENABLED = 'true';
+process.env.REDIS_HOST = 'localhost';
+process.env.REDIS_PORT = '6379';
+process.env.REDIS_PASSWORD = '';
+process.env.REDIS_USERNAME = '';
