d9063bc9f9262d1b9e006ce812d3d1242bf740d5
Elijah Arhinful <elijaharhinful8@gmail.com>
2026-05-18 20:20:35 +0000

    fix(contact): fix migration file for contact table

--- src/database/migrations/1779133609700-CreateContactTable.ts
+++ src/database/migrations/1779133609700-CreateContactTable.ts
@@ -1,14 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateContactTable1779133609700 implements MigrationInterface {
-    name = 'CreateContactTable1779133609700'
+  name = 'CreateContactTable1779133609700';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "contacts" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME 
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "contacts" (
+                "id" uuid NOT NULL DEFAULT uuid_generate_v4(), 
+                "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), 
+                "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+            );`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "contacts"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "contacts"`);
+  }
 }
