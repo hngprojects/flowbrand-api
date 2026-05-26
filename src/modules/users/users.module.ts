158e9417432822eb11f99e9e8a22d6adec72ab75
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-05-26 15:49:58 +0300

    feat(users): add repositories required for user state feature

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -6,11 +6,25 @@ import { UserSession } from './entities/user-session.entity';
 import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { RedisService } from './../redis/redis.service';
+import { WizardSession } from './../onboarding/entities/wizzard-session.entity';
+import { Funnel } from './../funnels/entities/funnel.entity';
+import { FunnelStage } from './../funnels/entities/funnel-stage.entity';
+import { StageTask } from './../funnels/entities/stage-task.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserSession])],
+  imports: [
+    TypeOrmModule.forFeature([
+      User, 
+      UserSession,
+      WizardSession,
+      Funnel,
+      FunnelStage,
+      StageTask,
+    ])
+  ],
   controllers: [UsersController],
-  providers: [UserModelAction, UserSessionModelAction, UsersService],
+  providers: [UserModelAction, UserSessionModelAction, UsersService, RedisService,],
   exports: [UsersService, UserModelAction, UserSessionModelAction],
 })
 export class UsersModule {}
