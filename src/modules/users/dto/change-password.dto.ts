c205cd9add007706e9b4edaf864256faf205b655
Prestige Nsien <prestigensien45@gmail.com>
2026-05-27 00:54:07 +0100

    fix(users): update new password validation message with a clearer message

--- src/modules/users/dto/change-password.dto.ts
+++ src/modules/users/dto/change-password.dto.ts
@@ -13,7 +13,7 @@ export class ChangePasswordDto {
   @MinLength(8)
   @MaxLength(128)
   @Matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~]).*$/, {
-    message: 'new_password must contain at least one uppercase letter, one lowercase letter, one number, and one ASCII symbol',
+    message: 'new_password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
   })
   newPassword: string;
 
