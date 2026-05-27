53ac732fc46529ea8d0a15e30768f7040d96f364
elijah arhinful <elijaharhinful8@gmail.com>
2026-05-27 14:21:26 +0000

    fix(funnels): resolve dependency issues in funnel module

--- src/modules/funnels/funnels.module.ts
+++ src/modules/funnels/funnels.module.ts
@@ -5,18 +5,16 @@ import { QUEUES } from '../../common/constants/queue.constants';
 import { WizardSession } from '../onboarding/entities/wizzard-session.entity';
 import { RedisModule } from '../redis/redis.module';
 import { UploadedDocument } from '../upload/entities/uploaded-document.entity';
-
 import { Funnel } from './entities/funnel.entity';
 import { FunnelStage } from './entities/funnel-stage.entity';
 import { StageTask } from './entities/stage-task.entity';
-
 import { FunnelsController } from './controllers/funnels.controller';
-
-// Providers / actions
 import { FunnelModelAction } from './actions/funnel.action';
 import { FunnelStageModelAction } from './actions/funnel-stage.action';
-
+import { StageTaskModelAction } from './actions/stage-task.action';
 import { FunnelsService } from './services/funnels.service';
+
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([Funnel, FunnelStage, StageTask, WizardSession, UploadedDocument]),
@@ -28,6 +26,7 @@ import { FunnelsService } from './services/funnels.service';
     FunnelsService,
     FunnelModelAction,
     FunnelStageModelAction,
+    StageTaskModelAction,
   ],
   exports: [FunnelsService, FunnelModelAction],
 })
