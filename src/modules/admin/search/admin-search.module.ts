2cbc4ba5036e74dc8db0dcdeac790c8190b959e5
elijah arhinful <elijaharhinful8@gmail.com>
2026-06-03 14:34:53 +0000

    fix(admin-search): resolve circular depe depency issue in module

--- src/modules/admin/search/admin-search.module.ts
+++ src/modules/admin/search/admin-search.module.ts
@@ -1,19 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../../users/entities/user.entity';
-import { UserRoleEntity } from '../../users/entities/user-role.entity';
-import { UsersModule } from '../../users/users.module';
-import { AdminSearchController } from './controllers//admin-search.controller';
+import { AdminAuthModule } from '../auth/admin-auth.module';
+import { AdminSearchController } from './controllers/admin-search.controller';
 import { AdminSearchService } from './services/admin-search.service';
 import { AdminSearchModelAction } from './actions/admin-search.action';
+import { RolesGuard } from '../../auth/guards/roles.guard';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, UserRoleEntity]),
-    UsersModule,
+    TypeOrmModule.forFeature([User]),
+    AdminAuthModule,
   ],
   controllers: [AdminSearchController],
-  providers: [AdminSearchService, AdminSearchModelAction],
+  providers: [AdminSearchService, AdminSearchModelAction, RolesGuard],
   exports: [AdminSearchService],
 })
 export class AdminSearchModule {}
